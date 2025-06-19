dde10ca75537840773d8ad6360debfed60d07f13
bjrivers12 <brivers6@gatech.edu>
2025-06-19 21:55:21 +0000

    Indirect recusion

--- main.c
+++ main.c
@@ -1,19 +1,26 @@
 #include <stdio.h>
-
-void fun(int n)
+void funB(int n);
+void funA(int n)
 {
-    if (n > 0)
+    if(n>0)
     {
-       printf("%d ",n);
-       fun(n-1);
-       fun(n-1);
-    };
+        printf("%d ",n);
+        funB(n-1);
+    }
+}
 
+void funB(int n)
+{
+    if(n>0)
+    {
+        printf("%d ",n);
+        funA(n/2);
+    }
 }
 
 int main()
 {
-    fun(3);
+    funA(20);
 
     return 0;
 }
