4cd9b91e869c5a7c55ba4180289c7d9e1a413827
Brian Rivers <brivers6@gatech.edu>
2025-06-23 02:11:53 +0000

    starting python

--- main.c
+++ main.c
@@ -2,19 +2,17 @@
 
 int fun(int n)
 {
-    if(n>100)
+    if (n > 100)
     {
-        return n-10;
-    } else {
-        return fun(fun(n+11));
+        return n - 10;
+    }
+    else
+    {
+        return fun(fun(n + 11));
     }
 }
 
-
 int main()
 {
-    int r;
-    r=fun(95);
-    printf("%d\n",r);
     return 0;
 }
