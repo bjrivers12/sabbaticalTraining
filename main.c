44fbfccec538678b2947a3e8f7e30b767c46c74c
bjrivers12 <brivers6@gatech.edu>
2025-06-22 19:16:32 -0700

    git stuff

--- main.c
+++ main.c
@@ -1,20 +1,27 @@
 #include <stdio.h>
 
-int fun(int n)
+int sum(int n)
 {
-    if(n>100)
-    {
-        return n-10;
-    } else {
-        return fun(fun(n+11));
-    }
+    if(n==0)
+        return 0;
+    return sum(n-1) + n;
+
+}
+
+int Isum(int n)
+{
+    int s=0;
+    int i;
+    for(i=1;i<=n;i++)
+        s=s+i;
+    return s;
 }
 
 
 int main()
 {
     int r;
-    r=fun(95);
+    r=Isum(5);
     printf("%d\n",r);
     return 0;
 }
