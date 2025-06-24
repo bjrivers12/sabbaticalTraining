2748117a2f1ad09ed6e37da543c21af8641b0853
bjrivers12 <brivers6@gatech.edu>
2025-06-24 03:43:12 +0000

    factorial

--- main.c
+++ main.c
@@ -1,18 +1,30 @@
 #include <stdio.h>
 
-int sum(int n)
+int fact(int n)
 {
-    if (n > 100)
+    if (n==0)
     {
-        return n - 10;
+        return 1;
     }
     else
     {
-        return fun(fun(n + 11));
+        return fact(n-1)*n;
     }
+}
+
+int Ifact(int n)
+{
+    int f=1;
+    int i;
+    for(i=1;i<=n;i++)
+    f=f*i;
+    return f;
 }
 
 int main()
 {
+    int r;
+    r=Ifact(5);
+    printf("%d ",r);
     return 0;
 }
