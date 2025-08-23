effee7c242d25d78fc79887eef7f68c78d8be14a
Brian Rivers <brivers6@gatech.edu>
2025-08-23 14:07:47 -0700

    snake

---
+++ snake_game/main.rs
@@ -0,0 +1,15 @@
+
+
+
+fn main(){
+    let mut message = "Hello World";
+    println!("Some text:{}",message);
+    message = "Hi there";
+    println!("Some text:{}",message);
+
+    let mut age = 19;
+    println!("Some text:{}",age);
+    age = 30;
+    println!("Some text:{}",age);
+
+}
