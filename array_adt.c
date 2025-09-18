e0685988ff47f5e5f1bd2dd6f94de64abca3c429
Brian Rivers <brivers6@gatech.edu>
2025-09-18 03:45:08 +0000

    add array method

--- array_adt.c
+++ array_adt.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include<stdlib.h>
+#include <stdlib.h>
 
 struct Array
 {
@@ -12,27 +12,27 @@ void Display(struct Array arr)
 {
 	int i;
 	printf("\nElements are\n");
-	for(i=0;i<arr.length;i++)
-		printf("%d",arr.A[i]);
+	for (i = 0; i < arr.length; i++)
+		printf("%d", arr.A[i]);
 }
 
 int main()
 {
 	struct Array arr;
-	int n,i;
+	int n, i;
 	printf("Enter size of an array");
-	scanf("%d",&arr.size);
-	arr.A=(int *)malloc(arr.size*sizeof(int));
+	scanf("%d", &arr.size);
+	arr.A = (int *)malloc(arr.size * sizeof(int));
 	arr.length = 0;
 
 	printf("Enter how many numbers");
-	scanf("%d",&n);
+	scanf("%d", &n);
 
 	printf("Enter all Elements\n");
-	for(i=0;i<n;i++)
-		scanf("%d",&arr.A[i]);
-	arr.length=n;
+	for (i = 0; i < n; i++)
+		scanf("%d", &arr.A[i]);
+	arr.length = n;
 
 	Display(arr);
 	return 0;
-				}
+}
