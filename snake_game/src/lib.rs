08f1ed9fac31c28df01edb921cbdbd6e3da76629
bjrivers12 <brivers6@gatech.edu>
2025-09-28 23:57:31 +0000

    add change_snake_dir

--- snake_game/src/lib.rs
+++ snake_game/src/lib.rs
@@ -3,8 +3,9 @@ use wasm_bindgen::prelude::*;
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
+#[wasm_bindgen]
 #[derive(PartialEq)]
-enum Direction {
+pub enum Direction {
     Up,
     Right,
     Down,
@@ -50,6 +51,10 @@ impl World {
 
     pub fn snake_head_idx(&self) -> usize {
         self.snake.body[0].0
+    }
+
+    pub fn change_snake_dir(&mut self, direction: Direction) {
+        self.snake.direction = direction;
     }
 
     pub fn update(&mut self) {
