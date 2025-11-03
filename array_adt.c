73f6a81ac7015ba34db562286fa91d20e05a49ad
bjrivers12 <brivers6@gatech.edu>
2025-11-03 00:10:55 +0000

    delete method for array

--- array_adt.c
+++ array_adt.c
@@ -3,7 +3,7 @@
 
 struct Array
 {
-	int *A;
+	int A[10];
 	int size;
 	int length;
 };
@@ -13,25 +13,49 @@ void Display(struct Array arr)
 	int i;
 	printf("\nElements are\n");
 	for (i = 0; i < arr.length; i++)
-		printf("%d", arr.A[i]);
+		printf("%d ", arr.A[i]);
+	printf("\n");
+}
+
+void Append(struct Array *arr, int x)
+{
+	if(arr->length<arr->size)
+		arr->A[arr->length++]=x;
+}
+
+void Insert(struct Array *arr, int index, int x)
+{
+	int i;
+	if(index>=0 && index <=arr->length)
+	{
+		for(i=arr->length; i>index;i--)
+			arr->A[i]=arr->A[i-1];
+		arr->A[index]=x;
+		arr->length++;
+	}
+}
+
+int Delete(struct Array *arr, int index)
+{
+	int x=0;
+	int i;
+	if(index>=0 && index<=arr->length)
+	{
+		x=arr->A[index];
+		for (i=index; i<arr->length-1;i++)
+			arr->A[i]=arr->A[i+1];
+		arr->length--;
+		return x;
+
+	}
+	return 0;
 }
 
 int main()
 {
-	struct Array arr;
-	int n, i;
-	printf("Enter size of an array");
-	scanf("%d", &arr.size);
-	arr.A = (int *)malloc(arr.size * sizeof(int));
-	arr.length = 0;
+	struct Array arr={{2,3,4,5,6},10,5};
 
-	printf("Enter how many numbers");
-	scanf("%d", &n);
-
-	printf("Enter all Elements\n");
-	for (i = 0; i < n; i++)
-		scanf("%d", &arr.A[i]);
-	arr.length = n;
+	printf("%d\n",Delete(&arr,4));
 
 	Display(arr);
 	return 0;
