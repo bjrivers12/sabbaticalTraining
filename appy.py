ac1f15a7269fe6a2711c4f38fb796d19566cd95e
Brian Rivers <brivers6@gatech.edu>
2025-12-24 15:43:46 -0500

    learning buttons

--- appy.py
+++ appy.py
@@ -1,8 +1,17 @@
 import tkinter as tk
 from tkinter import ttk
 
+
+def greet():
+    print("Hello, World!")
+
 root = tk.Tk()
-ttk.Label(root, text="Hello World").pack()
+
+greet_button = ttk.Button(root, text="Greet", command=greet)
+#expand true grows in the window fill makes it take up the reserved space and fill both takes up all space doesn't pprevent others from taking space
+greet_button.pack(side="left", fill="y", expand=True)
+quit_button = ttk.Button(root, text="Quit", command=root.destroy)
+quit_button.pack(side="left")
 
 root.mainloop()
 
