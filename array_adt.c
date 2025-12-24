e825a45622db849601552989f3516ea4da159f36
Brian Rivers <brivers6@gatech.edu>
2025-12-24 15:29:34 -0500

    gui tinkter

--- array_adt.c
+++ array_adt.c
@@ -19,43 +19,64 @@ void Display(struct Array arr)
 
 void Append(struct Array *arr, int x)
 {
-	if(arr->length<arr->size)
-		arr->A[arr->length++]=x;
+	if (arr->length < arr->size)
+		arr->A[arr->length++] = x;
 }
 
 void Insert(struct Array *arr, int index, int x)
 {
 	int i;
-	if(index>=0 && index <=arr->length)
+	if (index >= 0 && index <= arr->length)
 	{
-		for(i=arr->length; i>index;i--)
-			arr->A[i]=arr->A[i-1];
-		arr->A[index]=x;
+		for (i = arr->length; i > index; i--)
+			arr->A[i] = arr->A[i - 1];
+		arr->A[index] = x;
 		arr->length++;
 	}
 }
 
 int Delete(struct Array *arr, int index)
 {
-	int x=0;
+	int x = 0;
 	int i;
-	if(index>=0 && index<=arr->length)
+	if (index >= 0 && index <= arr->length)
 	{
-		x=arr->A[index];
-		for (i=index; i<arr->length-1;i++)
-			arr->A[i]=arr->A[i+1];
+		x = arr->A[index];
+		for (i = index; i < arr->length - 1; i++)
+			arr->A[i] = arr->A[i + 1];
 		arr->length--;
 		return x;
-
 	}
 	return 0;
 }
 
+void swap(int *x, int *y)
+{
+	int temp;
+	temp = *x;
+	*x = *y;
+	*y = temp;
+}
+
+int LinearSearch(struct Array *arr, int key)
+{
+	int i;
+	for (i = 0; i < arr->length; i++)
+	{
+		if (key == arr->A[i])
+		{
+			swap(&arr->A[i], &arr->A[i - 1]);
+			return i;
+		}
+	}
+	return -1;
+};
+
 int main()
 {
-	struct Array arr={{2,3,4,5,6},10,5};
+	struct Array arr = {{2, 3, 4, 5, 6}, 10, 5};
 
-	printf("%d\n",Delete(&arr,4));
+	printf("%d\n", LinearSearch(&arr, 4));
 
 	Display(arr);
 	return 0;
