a6bdb2100bc37a5155bfd608a46d207b5a8ba347
Brian Rivers <brivers6@gatech.edu>
2025-12-29 13:33:39 -0500

    new backend test

--- appy.py
+++ appy.py
@@ -3,9 +3,18 @@ from tkinter import ttk
 
 
 def greet():
-    print("Hello, World!")
+    print(f"Hello, {user_name.get() or 'World'}")
 
 root = tk.Tk()
+
+user_name = tk.StringVar()
+
+name_label = ttk.Label(root, text="Name: ")
+name_label.pack(side="left", padx=(0,10))
+name_entry = ttk.Entry(root, width=15, textvariable=user_name)
+name_entry.pack(side="left")
+name_entry.focus()
+
 
 greet_button = ttk.Button(root, text="Greet", command=greet)
 #expand true grows in the window fill makes it take up the reserved space and fill both takes up all space doesn't pprevent others from taking space
