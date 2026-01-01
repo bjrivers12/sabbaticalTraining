15ea8514c84dc4f30d2b4fef2606808699e7e2ec
Brian Rivers <brivers6@gatech.edu>
2025-12-31 22:38:40 -0500

    Binary search

--- array_adt.c
+++ array_adt.c
@@ -73,11 +73,45 @@ int LinearSearch(struct Array *arr, int key)
 	return -1;
 };
 
+int BinarySearch(struct Array arr, int key)
+{
+	int l, mid, h;
+	l = 0;
+	h = arr.length - 1;
+	while (l <= h)
+	{
+		mid = (l + h) / 2;
+		if (key == arr.A[mid])
+			return mid;
+		else if (key < arr.A[mid])
+			h = mid - 1;
+		else
+			l = mid + 1;
+	}
+	return -1;
+}
+
+int RBinarySearch(int a[], int l, int h, int key)
+{
+	int mid;
+	if (l <= h)
+	{
+		mid = (l + h) / 2;
+		if (key == a[mid])
+			return mid;
+		else if (key < a[mid])
+			return RBinarySearch(a, l, mid - 1, key);
+		else
+			return RBinarySearch(a, mid + 1, h, key);
+	}
+}
+
 int main()
 {
 	struct Array arr = {{2, 3, 4, 5, 6}, 10, 5};
 
-	printf("%d\n", LinearSearch(&arr, 4));
+	printf("%d\n", BinarySearch(arr, 2));
+	printf("%d\n", RBinarySearch(arr.A, 0, arr.length, 5));
 
 	Display(arr);
 	return 0;
